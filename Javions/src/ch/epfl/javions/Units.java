5f9cc6e9b91d026b2a56fee1fc94049c6f4002f6
fotadavid <114075687+fotadavid@users.noreply.github.com>
2023-02-21 11:48:01 +0100

    1

--- Javions/src/ch/epfl/javions/Units.java
+++ Javions/src/ch/epfl/javions/Units.java
@@ -1,16 +1,13 @@
 package ch.epfl.javions;
 
-
-
-
 public final class Units {
     private Units(){}
 
     public static final double CENTI = 1e-2;
     public static final double KILO = 1e3;
 
-    public static class Angles{
-        private Angles(){}
+    public static class Angle{
+        private Angle(){}
         public static final double RADIAN = 1;
         public static final double TURN = 2*Math.PI* RADIAN;
         public static final double DEGREE = TURN/360;
