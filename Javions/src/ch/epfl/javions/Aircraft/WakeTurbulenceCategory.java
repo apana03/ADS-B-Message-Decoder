8e8ec4d684890b86a6eb0666afac56ea10447452
apana03 <andrei.pana@epfl.ch>
2023-02-26 16:01:46 +0200

    WakeTurbulenceCategory enum

---
+++ Javions/src/ch/epfl/javions/Aircraft/WakeTurbulenceCategory.java
@@ -0,0 +1,35 @@
+package ch.epfl.javions.Aircraft;
+
+/**
+ *Represents the wake turbulence category of an aircraft
+ *
+ * @author Andrei Pana
+ * @author David Fota
+ */
+
+public enum WakeTurbulenceCategory {
+    LIGHT,
+    MEDIUM,
+    HEAVY,
+    UNKNOWN;
+
+    /**
+     * @returns the wake turbulence category corresponding to the given string
+     * @param s the string
+     */
+    public static WakeTurbulenceCategory of(String s){
+        switch (s){
+            case "L" -> {
+                return LIGHT;
+            }
+            case "M" -> {
+                return MEDIUM;
+            }
+            case "H" -> {
+                return HEAVY;
+            }
+
+        }
+        return UNKNOWN;
+    }
+}
