323ec8aae9159a02a7c6f4b1c9707e376fc23f70
fotadavid <114075687+fotadavid@users.noreply.github.com>
2023-03-01 16:55:56 +0100

    +AircraftData +AircraftDatabase -Crc24

--- Javions/src/ch/epfl/javions/aircraft/AircraftDatabase.java
+++ Javions/src/ch/epfl/javions/aircraft/AircraftDatabase.java
@@ -1,6 +1,68 @@
 package ch.epfl.javions.aircraft;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.ZipFile;
+
+/**
+ * Represents an aircraft database
+ * @author David Fota
+ * @author Andrei Pana
+ */
 public class AircraftDatabase
 {
-
+    String fileName, name;
+    String[] data;
+    AircraftData finalData;
+    char[] hashMap = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'};
+    /**
+     * @param fileName
+     *        the string corresponding to the name of the file
+     *        in which the database is stored
+     * @throws NullPointerException if the string is null
+     */
+    public AircraftDatabase(String fileName)
+    {
+        if( fileName == null )
+            throw new NullPointerException();
+        else
+            this.fileName = fileName;
+    }
+    /**
+     *@returns the desired aircraft's data, which is
+     *         extracted from the database
+     *@param address
+     *         corresponds to the ICAO address of the aircraft
+     *         we are looking for
+     * This method iterates on every file present in the database
+     * in order to find and extract the required information
+     */
+    public AircraftData get(IcaoAddress address) throws IOException
+    {
+        String d = getClass().getResource(fileName).getFile();
+        for(char c1 : hashMap)
+            for(char c2 : hashMap)
+            {
+                name = c1 + c2 + ".csv";
+                try (ZipFile z = new ZipFile(d);
+                     InputStream s = z.getInputStream(z.getEntry(name));
+                     Reader r = new InputStreamReader(s, StandardCharsets.UTF_8);
+                     BufferedReader b = new BufferedReader(r))
+                {
+                    String l = "";
+                    while ((l = b.readLine()).compareTo(address.string()) <= 0)
+                    {
+                        if (l.startsWith(address.string()))
+                        {
+                            data = l.split(",");
+                            finalData = new AircraftData(new AircraftRegistration(data[1]),
+                                    new AircraftDescription(data[4]), new AircraftTypeDesignator(data[2]),
+                                    data[3], WakeTurbulenceCategory.of(data[5]));
+                            return finalData;
+                        }
+                    }
+                }
+            }
+        return null;
+    }
 }
