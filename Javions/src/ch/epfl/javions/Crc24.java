3dcd71d217de744013f65bf4a02ee9d29516b066
apana03 <andrei.pana@epfl.ch>
2023-03-02 22:08:31 +0200

    crcbitwise

--- Javions/src/ch/epfl/javions/Crc24.java
+++ Javions/src/ch/epfl/javions/Crc24.java
@@ -33,7 +33,7 @@ public final class Crc24 {
        return crc & 0xFFFFFF;
    }
 
-    private  int crc_bitwise(int generator, byte[]bytes) {
+    /*private  int crc_bitwise(int generator, byte[]bytes) {
         int leastSignificantBits = generator & 0xFFFFFF;
         int table[] = new int[]{0, leastSignificantBits};
         int crc = 0;
@@ -48,6 +48,21 @@ public final class Crc24 {
             for(int i = 1 ; i < 8 ; i<<=1) {
                 int bit = b&i;
                 crc = ((crc << 1) | bit) ^ table[(crc & 4194304) >> 22];
+            }
+        }
+        return crc & 0xFFFFFF;
+    }
+
+     */
+    private static int crc_bitwise(int generator, byte[] data) {
+        int crc = 0x000000;
+        for (byte b : data) {
+            crc ^= (b & 0xFF) << 16;
+            for (int i = 0; i < 8; i++) {
+                crc <<= 1;
+                if ((crc & 0x1000000) != 0) {
+                    crc ^= generator;
+                }
             }
         }
         return crc & 0xFFFFFF;
