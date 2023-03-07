ce2dd77ac16639d61c56f1687a32ef003d377703
fotadavid <114075687+fotadavid@users.noreply.github.com>
2023-03-07 14:44:25 +0100

    +SamplesDecoder +PowerComputer

--- Javions/src/ch/epfl/javions/demodulation/PowerComputer.java
+++ Javions/src/ch/epfl/javions/demodulation/PowerComputer.java
@@ -4,13 +4,24 @@ import ch.epfl.javions.Preconditions;
 
 import java.io.IOException;
 import java.io.InputStream;
-
+/**
+ * Represents a power computer
+ * @author David Fota 355816
+ * @author Andrei Pana 361249
+ */
 public class PowerComputer
 {
     private SamplesDecoder decoder;
     private short[] signedBatch;
     private short[] samples = new short[8];
     private int batchSize;
+    /**
+     * @param stream, batchSize
+     *        The input stream received by the decoder
+     *        Size of the batch that is to be computed
+     * @throws IllegalArgumentException if the size of the batch
+     * is not multiple of 8 or if it is negative
+     */
     public PowerComputer(InputStream stream, int batchSize)
     {
         Preconditions.checkArgument((batchSize * 4) % 8 == 0 && batchSize > 0);
@@ -19,6 +30,18 @@ public class PowerComputer
         this.batchSize = batchSize;
         signedBatch = new short[batchSize*2];
     }
+    /**
+     * @param batch
+     * reads from the sample decoder the number of
+     * samples needed to calculate a batch of power
+     * samples, then calculates them using the given
+     * formula and places them in the array passed as argument
+     * @return the number of power samples that have been placed
+     * in the table
+     * @throws IOException in case of input/output error
+     * @throws IllegalArgumentException if the size of the table
+     * passed as argument is not equal to the size of a batch
+     */
     public int readBatch( int batch[] ) throws IOException
     {
         Preconditions.checkArgument( batchSize == batch.length);
--- Javions/src/ch/epfl/javions/demodulation/SamplesDecoder.java
+++ Javions/src/ch/epfl/javions/demodulation/SamplesDecoder.java
@@ -5,12 +5,25 @@ import ch.epfl.javions.Preconditions;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.HexFormat;
-
+/**
+ * Represents a sample decoder
+ * transforming received octets
+ * into 12-bit signed samples
+ * @author David Fota 355816
+ * @author Andrei Pana 361249
+ */
 public final class SamplesDecoder
 {
-    InputStream stream;
-    int batchSize;
-    byte[] octets;
+    private InputStream stream;
+    private int batchSize;
+    private byte[] octets;
+    /**
+     * @param stream, batchSize
+     *        The input stream received by the decoder
+     *        Size of the batch that is to be decoded
+     * @throws IllegalArgumentException if batchSize is 0 or negative
+     * @throws NullPointerException if the received stream is null
+     */
     public SamplesDecoder(InputStream stream, int batchSize)
     {
         Preconditions.checkArgument(batchSize > 0);
@@ -19,6 +32,19 @@ public final class SamplesDecoder
         this.stream = stream;
         this.batchSize = batchSize;
     }
+
+    /**
+     * @param batch
+     * reads from the stream passed to the constructor
+     * the number of bytes corresponding to a batch,
+     * then converts these bytes into signed samples,
+     * which are placed in the array passed as argument
+     * @return the size of the batch if it was big enough
+     * for all the octets to be read, and the number of octets
+     * that have been read divided by 2 and rounded to floor
+     * otherwise
+     * @throws IOException
+     */
     public int readBatch(short[] batch) throws IOException
     {
         Preconditions.checkArgument(batchSize == batch.length);
