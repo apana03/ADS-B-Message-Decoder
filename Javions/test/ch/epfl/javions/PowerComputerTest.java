2c9900250cc9114b830f626235b2dbf96f467a41
fotadavid <114075687+fotadavid@users.noreply.github.com>
2023-03-08 10:58:48 +0100

    +SamplesDecoder +PowerComputer +PowerWindow with all tests

--- Javions/test/ch/epfl/javions/PowerComputerTest.java
+++ Javions/test/ch/epfl/javions/PowerComputerTest.java
@@ -4,12 +4,10 @@ import ch.epfl.javions.demodulation.PowerComputer;
 import ch.epfl.javions.demodulation.SamplesDecoder;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PowerComputerTest
 {
@@ -28,4 +26,52 @@ public class PowerComputerTest
         for( int i = 0; i < 10; i++)
             assertEquals(expected[i], batch[i]);
     }
+    @Test
+    public void testConstructorInvalidBatchSize() {
+        assertThrows(IllegalArgumentException.class, () -> new PowerComputer(null, 5));
+    }
+
+
+    @Test
+    public void testReadBatchInvalidBatchSize() throws IOException {
+        // Initialise un PowerComputer avec un lot de taille 16
+        ByteArrayInputStream input = new ByteArrayInputStream(new byte[0]);
+        PowerComputer computer = new PowerComputer(input, 16);
+
+
+        // Vérifie que readBatch lève une exception si le tableau passé en argument n'a pas la bonne taille
+        assertThrows(IllegalArgumentException.class, () -> computer.readBatch(new int[10]));
+    }
+    @Test
+    public void testConstructorIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> new PowerComputer(new ByteArrayInputStream(new byte[0]), 7));
+    }
+
+
+    @Test
+    public void testReadBatchIllegalArgumentException() throws IOException {
+        PowerComputer pc = new PowerComputer(new ByteArrayInputStream(new byte[8]), 8);
+        assertThrows(IllegalArgumentException.class, () -> pc.readBatch(new int[7]));
+    }
+
+
+    @Test
+    public void testConstructorInvalidBatchSize1() {
+        InputStream stream = new ByteArrayInputStream(new byte[100]);
+        assertThrows(IllegalArgumentException.class, () -> new PowerComputer(stream, 5));
+    }
+
+
+    @Test
+    public void testConstructorIllegalArgumentException1() {
+        assertThrows(IllegalArgumentException.class, () -> new PowerComputer(new ByteArrayInputStream(new byte[0]), 7));
+    }
+
+
+    @Test
+    public void testReadBatchIllegalArgumentException1() throws IOException {
+        PowerComputer pc = new PowerComputer(new ByteArrayInputStream(new byte[8]), 8);
+        assertThrows(IllegalArgumentException.class, () -> pc.readBatch(new int[7]));
+    }
+
 }
