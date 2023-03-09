a285a073985e52c75559850a79cdda41a26e20f5
apana03 <andrei.pana@epfl.ch>
2023-03-10 00:15:33 +0200

    power window start of fix

--- Javions/src/ch/epfl/javions/demodulation/PowerWindow.java
+++ Javions/src/ch/epfl/javions/demodulation/PowerWindow.java
@@ -13,6 +13,8 @@ import java.io.InputStream;
  */
 public final class PowerWindow {
 
+    private static final int WINDOW_MAX_SIZE = 1<<16;
+
     private int windowSize;
     private long position = 0;
     private long samplesDecoded =0;
@@ -23,11 +25,11 @@ public final class PowerWindow {
 
 
     public PowerWindow(InputStream stream,int windowSize) throws IOException{
-        Preconditions.checkArgument(windowSize>0 && windowSize<=65536);
+        Preconditions.checkArgument(windowSize>0 && windowSize<=WINDOW_MAX_SIZE);
         this.windowSize = windowSize;
-        batch1 = new int[windowSize];
-        batch2 = new int[windowSize];
-        powerComputer = new PowerComputer(stream,windowSize);
+        batch1 = new int[WINDOW_MAX_SIZE];
+        batch2 = new int[WINDOW_MAX_SIZE];
+        powerComputer = new PowerComputer(stream,WINDOW_MAX_SIZE);
         samplesDecoded+=powerComputer.readBatch(batch1);
     }
 
@@ -50,16 +52,12 @@ public final class PowerWindow {
         if(i<0 && i>=windowSize){
             throw new IndexOutOfBoundsException();
         }
-        int positionStart = (int) position%windowSize;
-        if(positionStart == 0){
-            return batch1[i];
-        }else{
-            if(positionStart+i<=windowSize){
+        int positionStart = (int) position%WINDOW_MAX_SIZE ;
+            if(positionStart+i<WINDOW_MAX_SIZE){
                 return batch1[i];
             }else{
-                return batch2[(positionStart+i)%windowSize];
+                return batch2[(positionStart+i)%WINDOW_MAX_SIZE];
             }
-        }
     }
 
     public void advance() throws IOException{
@@ -67,7 +65,7 @@ public final class PowerWindow {
             samplesDecoded+=powerComputer.readBatch(batch2);
         }
         position++;
-        if(position%windowSize == 0){
+        if(position%WINDOW_MAX_SIZE == 0){
             batch1 = batch2;
             samplesDecoded+=powerComputer.readBatch(batch2);
         }
