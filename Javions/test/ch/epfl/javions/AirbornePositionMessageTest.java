8f02ef5c293ea365b4805656d929f7f871ecf1ad
fotadavid <114075687+fotadavid@users.noreply.github.com>
2023-03-24 14:44:13 +0100

    FINAL

--- Javions/test/ch/epfl/javions/AirbornePositionMessageTest.java
+++ Javions/test/ch/epfl/javions/AirbornePositionMessageTest.java
@@ -3,14 +3,21 @@ package ch.epfl.javions;
 
 import ch.epfl.javions.ByteString;
 import ch.epfl.javions.adsb.AirbornePositionMessage;
+import ch.epfl.javions.adsb.AircraftIdentificationMessage;
 import ch.epfl.javions.adsb.RawMessage;
 import ch.epfl.javions.aircraft.IcaoAddress;
+import ch.epfl.javions.demodulation.AdsbDemodulator;
 import org.junit.jupiter.api.Test;
 
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLDecoder;
 import java.util.HexFormat;
 
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.*;
 
 
@@ -236,5 +243,22 @@ public class AirbornePositionMessageTest {
         assertThrows(IllegalArgumentException.class, () -> {
             new AirbornePositionMessage(System.nanoTime(), icaoAddress, 1000.0, 0, 0.5, 1.5);
         });
+    }
+    @Test
+    void testFirst5Values() throws IOException {
+        String f = URLDecoder.decode(getClass().getResource("/samples_20230304_1442.bin").getFile(), UTF_8);
+        InputStream stream = new FileInputStream(f);
+        AdsbDemodulator d = new AdsbDemodulator(stream);
+        RawMessage m;
+        int mesCreated = 0;
+        while ((m = d.nextMessage()) != null) {
+            if (m.typeCode() >= 1 && m.typeCode() <= 4) {
+                var aux = AirbornePositionMessage.of(m);
+                if (aux != null)
+                    System.out.println(aux + " " + m);
+                mesCreated++;
+            }
+        }
+        System.out.println(mesCreated);
     }
 }
