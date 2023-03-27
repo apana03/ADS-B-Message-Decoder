faf16d10c632fef5cd6519e9632eb04a9c2f2e95
apana03 <andrei.pana@epfl.ch>
2023-03-27 19:44:47 +0300

    AirborneVelocityMessage

--- Javions/src/ch/epfl/javions/adsb/AirborneVelocityMessage.java
+++ Javions/src/ch/epfl/javions/adsb/AirborneVelocityMessage.java
@@ -2,9 +2,12 @@ package ch.epfl.javions.adsb;
 
 import ch.epfl.javions.Bits;
 import ch.epfl.javions.Preconditions;
+import ch.epfl.javions.Units;
 import ch.epfl.javions.aircraft.IcaoAddress;
 
-public record AirborneVelocityMessage(long timeStampNs, IcaoAddress icaoAddress, double speed, double trackOrHeading)implements Message {
+public record AirborneVelocityMessage(long timeStampNs,
+                                      IcaoAddress icaoAddress,
+                                      double speed, double trackOrHeading)implements Message {
     public AirborneVelocityMessage{
         if(icaoAddress == null){
             throw new NullPointerException();
@@ -17,23 +20,63 @@ public record AirborneVelocityMessage(long timeStampNs, IcaoAddress icaoAddress,
     AirborneVelocityMessage of(RawMessage rawMessage){
         long payload = rawMessage.payload();
         int st = Bits.extractUInt(payload,48,3);
+        int stBits = Bits.extractUInt(payload , 21 ,22 );
+        double speed;
+        double trackOrHeading;
+
         if ( st < 1 || st > 4 ){
             return null;
         }
+
         switch (st){
             case 1,2:
-                int stBits = Bits.extractUInt(payload , 21 ,22 );
+
                 int vns = Bits.extractUInt(stBits , 0 ,10 );
-                int dns = Bits.extractUInt(payload , 10 ,1 );
-                int vew = Bits.extractUInt(payload , 11 ,10 );
-                int dew = Bits.extractUInt(payload , 21 ,1 );
+                int dns = Bits.extractUInt(stBits , 10 ,1 );
+                int vew = Bits.extractUInt(stBits , 11 ,10 );
+                int dew = Bits.extractUInt(stBits , 21 ,1 );
 
                 if(vns == 0 || vew == 0){
                     return null;
                 }
 
-                double speed = Math.sqrt( Math.pow ( vns - 1 , 2 ) + Math.scalb( vew - 1 , 2 ) );
-                double trackOrHeading
+                vns = (dns > 0) ? (--vns) : (--vns)*(-1) ;
+                vew = (dew > 0) ? (--vew) : (--vew)*(-1) ;
+
+                speed = Math.sqrt( Math.pow ( vns , 2 ) + Math.scalb( vew , 2 ) );
+                speed = (st == 1) ? Units.convertFrom( speed, Units.Speed.KNOT):
+                        Units.convertFrom(4*speed, Units.Speed.KNOT);
+
+                trackOrHeading = Math.atan2( vns , vew );
+                trackOrHeading = (trackOrHeading<0) ? trackOrHeading + Math.PI*2 : trackOrHeading;
+
+                return new AirborneVelocityMessage(rawMessage.timeStampNs(),
+                        rawMessage.icaoAddress(),
+                        speed,
+                        trackOrHeading);
+
+            case 3, 4:
+
+                int as = Bits.extractUInt(stBits, 0, 10);
+                int hdg = Bits.extractUInt(stBits, 11, 10);
+                int sh = Bits.extractUInt(stBits, 21, 1);
+
+                if(sh == 0){
+                    return null;
+                }
+
+                speed = (st == 3) ? Units.convertFrom( as, Units.Speed.KNOT):
+                        Units.convertFrom(4 * as, Units.Speed.KNOT);
+
+                // converting cap to TURN
+                 trackOrHeading = hdg / Math.scalb(1,10);
+                 trackOrHeading = Units.convertFrom( trackOrHeading , Units.Angle.TURN );
+
+                return new AirborneVelocityMessage(rawMessage.timeStampNs(),
+                        rawMessage.icaoAddress(),
+                        speed,
+                        trackOrHeading);
         }
+        return null;
     }
 }
