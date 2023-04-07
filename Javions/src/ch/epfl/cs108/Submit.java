7c3b926f8520961bffd1e5b9393680f1622ca7f9
fotadavid <114075687+fotadavid@users.noreply.github.com>
2023-04-07 10:57:43 +0200

    FINAL

--- Javions/src/ch/epfl/cs108/Submit.java
+++ Javions/src/ch/epfl/cs108/Submit.java
@@ -32,9 +32,9 @@ public final class Submit {
     // CONFIGURATION
     // -------------
     // Jeton du premier membre du groupe
-    private static final String TOKEN_1 = "ahJahd4z";
+    private static final String TOKEN_1 = "Pii3jaid";
     // Jeton du second membre (identique au premier pour les personnes travaillant seules)
-    private static final String TOKEN_2 = "Lu0ailie";
+    private static final String TOKEN_2 = "jeicie1I";
     // -------------
 
     private static final String ZIP_ENTRY_NAME_PREFIX = "Javions/";
