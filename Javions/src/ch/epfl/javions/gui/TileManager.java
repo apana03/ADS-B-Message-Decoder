ff88fe417f1397086ea07e967d62d2c4c4b8d816
fotadavid <114075687+fotadavid@users.noreply.github.com>
2023-04-26 13:53:52 +0200

    FINAL

---
+++ Javions/src/ch/epfl/javions/gui/TileManager.java
@@ -0,0 +1,10 @@
+package ch.epfl.javions.gui;
+
+public final class TileManager {
+    public record TileId(int z, int x, int y){
+        public boolean isValid(){
+            if()
+        }
+    }
+
+}
