1b5df7108cccf847207390d954f2591e74422acd
fotadavid <114075687+fotadavid@users.noreply.github.com>
2023-04-26 15:21:42 +0200

    FINAL

--- Javions/src/ch/epfl/javions/gui/TileManager.java
+++ Javions/src/ch/epfl/javions/gui/TileManager.java
@@ -2,14 +2,19 @@ package ch.epfl.javions.gui;
 
 import javafx.scene.image.Image;
 
+import javax.print.DocFlavor;
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 
 public final class TileManager {
-    public record TileId(int z, int x, int y){
+    public record TileId(int zoom, int x, int y){
         public boolean isValid(){
-            return (x >= 0) && (x < (1 << z)) && (y >= 0) && (y < (1 << z));
+            return (x >= 0) && (x < (1 << zoom)) && (y >= 0) && (y < (1 << zoom));
         }
     }
     private final Path localCache;
@@ -22,7 +27,33 @@ public final class TileManager {
         this.serverUrl = serverUrl;
         cache = new LinkedHashMap<>(MAX_CACHE_CAPACITY, LOAD_FACTOR, true);
     }
-    public Image imageForTileAt(TileId tileId){
-        return null;
+    public Image imageForTileAt(TileId tileId) throws IOException {
+        if(cache.containsKey(tileId)){
+            return cache.get(tileId);
+        }
+        Path path = Path.of(tileId.zoom() + "/" + tileId.x() + "/" + tileId.y() + ".png");
+        if(cache.size() == MAX_CACHE_CAPACITY){
+            cache.remove(cache.keySet().iterator().next());
+        }
+        if(Files.exists(path)){
+            try(FileInputStream is = new FileInputStream(path.toFile())){
+                Image image = new Image(is);
+                cache.put(tileId,image);
+                return image;
+        }
+        }else{
+            URL u = new URL(serverUrl + "/" + tileId.zoom() + "/" + tileId.x() + "/" + tileId.y() + ".png");
+            URLConnection c = u.openConnection();
+            Files.createDirectories(path.getParent());
+            c.setRequestProperty("User-Agent", "Javions");
+            try(InputStream is = c.getInputStream()){
+                byte[] buffer = is.readAllBytes();
+                FileOutputStream o = new FileOutputStream(path.toFile());
+                o.write(buffer);
+                Image image = new Image(new ByteArrayInputStream(buffer));
+                cache.put(tileId , image);
+                return image;
+            }
+        }
     }
 }
