df0b31a97d0e363ce3f719f8392804e1ef02bfd8
apana03 <andrei.pana@epfl.ch>
2023-04-28 14:52:10 +0300

    centerOn + Map Parameters change in construct

--- Javions/src/ch/epfl/javions/gui/BaseMapController.java
+++ Javions/src/ch/epfl/javions/gui/BaseMapController.java
@@ -1,5 +1,9 @@
 package ch.epfl.javions.gui;
 
+import ch.epfl.javions.GeoPos;
+import ch.epfl.javions.WebMercator;
+import javafx.application.Platform;
+import javafx.beans.property.DoubleProperty;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.Pane;
 
@@ -8,24 +12,57 @@ import java.io.IOException;
 public final class BaseMapController {
     TileManager tileManager;
     MapParameters mapParameters;
-
+    private Canvas canvas;
+    private Pane pane;
     private boolean redrawNeeded;
+
+    private static final int PIXELS_IN_TILE = 256;
 
     public BaseMapController(TileManager tileManager, MapParameters mapParameters) {
         this.tileManager = tileManager;
         this.mapParameters = mapParameters;
+
+        canvas = new Canvas();
+        pane = new Pane(canvas);
+        canvas.widthProperty().bind(pane.widthProperty());
+
+        canvas.sceneProperty().addListener((p, oldS, newS) -> {
+            assert oldS == null;
+            newS.addPreLayoutPulseListener(this::redrawIfNeeded);
+        });
     }
 
     public Pane pane() {
-        Canvas canvas = new Canvas();
-        Pane pane = new Pane(canvas);
-        canvas.widthProperty().bind(pane.widthProperty());
-        int minX = (int) Math.floor(mapParameters.getMinXValue() / 256);
-        int minY = (int) Math.floor(mapParameters.getMinYValue() / 256);
-        double firstTileXPixel = -(mapParameters.getMinXValue()%256);
-        double firstTileYPixel = -(mapParameters.getMinYValue()%256);
-        for (int i = minX; i < minX + Math.floor(pane.getWidth() / 256); i++) {
-            for (int j = minY; j < minY + Math.floor(pane.getHeight() / 256); j++) {
+        drawMap(pane);
+        return pane;
+    }
+
+    public void centerOn(GeoPos point){
+        int zoomLvl = mapParameters.getZoomValue();
+        double x = WebMercator.x(zoomLvl, point.longitude());
+        double y = WebMercator.y(zoomLvl, point.latitude());
+        mapParameters = new MapParameters(zoomLvl, x - (canvas.getWidth()/2) , y-(canvas.getHeight()/2));
+        redrawOnNextPulse();
+    }
+
+    private void redrawIfNeeded() {
+        if (!redrawNeeded) return;
+        redrawNeeded = false;
+        drawMap(pane);
+    }
+
+    private void redrawOnNextPulse() {
+        redrawNeeded = true;
+        Platform.requestNextPulse();
+    }
+
+    private void drawMap(Pane pane){
+        int minX = (int) Math.floor(mapParameters.getMinXValue() / PIXELS_IN_TILE);
+        int minY = (int) Math.floor(mapParameters.getMinYValue() / PIXELS_IN_TILE);
+        double firstTileXPixel = -(mapParameters.getMinXValue()%PIXELS_IN_TILE);
+        double firstTileYPixel = -(mapParameters.getMinYValue()%PIXELS_IN_TILE);
+        for (int i = minX; i < minX + Math.floor(pane.getWidth() / PIXELS_IN_TILE); i++) {
+            for (int j = minY; j < minY + Math.floor(pane.getHeight() / PIXELS_IN_TILE); j++) {
                 try {
                     canvas.getGraphicsContext2D().drawImage(
                             tileManager.imageForTileAt(new TileManager.TileId(mapParameters.getZoomValue(), i, j))
@@ -34,16 +71,9 @@ public final class BaseMapController {
                 } catch (IOException e) {
 
                 }
-                    firstTileXPixel+=256;
-                }
-            firstTileYPixel+=256;
+                firstTileXPixel+=PIXELS_IN_TILE;
             }
-        return pane;
-    }
-
-    private void redrawIfNeeded() {
-        if (!redrawNeeded) return;
-        redrawNeeded = false;
-
+            firstTileYPixel+=PIXELS_IN_TILE;
+        }
     }
 }
--- Javions/src/ch/epfl/javions/gui/MapParameters.java
+++ Javions/src/ch/epfl/javions/gui/MapParameters.java
@@ -2,20 +2,17 @@ package ch.epfl.javions.gui;
 
 import ch.epfl.javions.Math2;
 import ch.epfl.javions.Preconditions;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.ReadOnlyDoubleProperty;
-import javafx.beans.property.ReadOnlyIntegerProperty;
+import javafx.beans.property.*;
 
 public final class MapParameters {
     private IntegerProperty zoom;
     private DoubleProperty minX,minY;
     private final static int MIN_ZOOM = 6, MAX_ZOOM =19;
-    public MapParameters(IntegerProperty zoom , DoubleProperty minX, DoubleProperty minY){
-        Preconditions.checkArgument(zoom.get() >= MIN_ZOOM || zoom.get()<= MAX_ZOOM);
-        this.zoom = zoom;
-        this.minX =minX;
-        this.minY = minY;
+    public MapParameters(int zoom , double minX, double minY){
+        Preconditions.checkArgument(zoom >= MIN_ZOOM && zoom <= MAX_ZOOM);
+        this.zoom = new SimpleIntegerProperty(zoom);
+        this.minX = new SimpleDoubleProperty(minX);
+        this.minY = new SimpleDoubleProperty(minY);
     }
 
     public ReadOnlyIntegerProperty getZoom(){
