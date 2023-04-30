5cd9e2c0797df652cbecdeb289fe08c563beab68
apana03 <andrei.pana@epfl.ch>
2023-04-30 12:24:02 +0300

    zoom in works but zoom out doesnt

--- Javions/src/ch/epfl/javions/gui/MapParameters.java
+++ Javions/src/ch/epfl/javions/gui/MapParameters.java
@@ -6,43 +6,62 @@ import javafx.beans.property.*;
 
 public final class MapParameters {
     private IntegerProperty zoom;
-    private DoubleProperty minX,minY;
-    private final static int MIN_ZOOM = 6, MAX_ZOOM =19;
-    public MapParameters(int zoom , double minX, double minY){
+    private DoubleProperty minX, minY;
+    private final static int MIN_ZOOM = 6, MAX_ZOOM = 19;
+
+    public MapParameters(int zoom, double minX, double minY) {
         Preconditions.checkArgument(zoom >= MIN_ZOOM && zoom <= MAX_ZOOM);
         this.zoom = new SimpleIntegerProperty(zoom);
         this.minX = new SimpleDoubleProperty(minX);
         this.minY = new SimpleDoubleProperty(minY);
     }
 
-    public ReadOnlyIntegerProperty getZoom(){
+    public ReadOnlyIntegerProperty getZoom() {
         return zoom;
     }
 
-    public int getZoomValue(){
+    public int getZoomValue() {
         return zoom.get();
     }
 
-    public ReadOnlyDoubleProperty getMinX(){
+    public ReadOnlyDoubleProperty getMinX() {
         return minX;
     }
-    public double getMinXValue(){
+
+    public double getMinXValue() {
         return minX.get();
     }
 
-    public ReadOnlyDoubleProperty getMinY(){
+    public ReadOnlyDoubleProperty getMinY() {
         return minY;
     }
-    public double getMinYValue(){
+
+    public double getMinYValue() {
         return minY.get();
     }
 
-    public void scroll(double x, double y){
+    public void scroll(double x, double y) {
         minX.set(minX.get() + x);
         minY.set(minY.get() + y);
     }
 
-    public void changeZoomLevel(int zoomDifference){
-        zoom.set(Math2.clamp(MIN_ZOOM,zoom.get()+zoomDifference,MAX_ZOOM));
+    public void changeZoomLevel(int zoomDifference) {
+        int previousZoom = zoom.get();
+        zoom.set(Math2.clamp(MIN_ZOOM, previousZoom + zoomDifference, MAX_ZOOM));
+        zoomDifference = previousZoom - zoom.get();
+        adaptTopLeftCorner(zoomDifference);
+    }
+
+    private void adaptTopLeftCorner(int zoomDifference) {
+        double var;
+        if (zoomDifference > 0) {
+            var = Math.pow(2, zoomDifference);
+        } else if (zoomDifference < 0) {
+            var = 1 / Math.pow(2, zoomDifference);
+        } else {
+            var = 1;
+        }
+        minX.set(minX.get() * var);
+        minY.set(minY.get() * var);
     }
 }
