3d42943e2c3c2445c3958f3fc1b5eee479ed008f
apana03 <andrei.pana@epfl.ch>
2023-05-19 16:27:43 +0300

    java doc in interfaces

--- Javions/src/ch/epfl/javions/adsb/Message.java
+++ Javions/src/ch/epfl/javions/adsb/Message.java
@@ -1,18 +1,25 @@
-package ch.epfl.javions.adsb;/*
- * Author: Andrei Pana
- * Date:
- */
+package ch.epfl.javions.adsb;
 
 import ch.epfl.javions.aircraft.IcaoAddress;
 
 /**
- * Defines the regular expression for Message
- *
+ * The Message interface represents a generic message.
  * @author Andrei Pana 361249
  * @author David Fota 355816
  */
 public interface Message {
-    public long timeStampNs();
 
-    public IcaoAddress icaoAddress();
+    /**
+     * Returns the timestamp in nanoseconds of the message.
+     *
+     * @return the timestamp in nanoseconds
+     */
+     long timeStampNs();
+
+    /**
+     * Returns the ICAO address associated with the message.
+     *
+     * @return the IcaoAddress object representing the ICAO address
+     */
+     IcaoAddress icaoAddress();
 }
